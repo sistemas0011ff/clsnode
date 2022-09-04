d153f72292f997ca5bf1fd1c09e4b8c43381bfe5
ARTURO EDUARDO FAJARDO GUTIÉRREZ (EXT) <efajardog-e@ripley.com.pe>
2022-09-04 11:10:59 -0500

    Funciones arrow básicas

--- src/index.ts
+++ src/index.ts
@@ -23,37 +23,24 @@ const routes =
         }
     ];
 
-
-//Función para leer rutas
-// const getRoute = (path : string): any => {
-//     return routes.find(route => route.path === path)
-// }
-
+//// Función para buscsar rutas    
 const getRoute = (path : string): any =>  routes.find(route => route.path === path)
 
+//// Creando exceptions
+const exceptionNotFound = (request: http.IncomingMessage, response: http.ServerResponse) =>{
+    response.writeHead(404, { "content-type": "text/plain" });
+    response.write("Error - url no encontrada");
+    response.end();
+}
 
 const server = http.createServer((request: http.IncomingMessage, response: http.ServerResponse) => {
-
-
-    //console.log(parseFloat('123456789101112.213213'));
-
-    console.log("reues", request.url);
-
-    if (request.url === "/usuario") {
-        response.writeHead(200, { "content-type": "text/plain" });
-        response.write("the app is running - usuarios");
-        response.end();
+    const route = getRoute(request.url as string);
+    
+    if (route){
+        route.excute(request,response);
+    }else{
+        exceptionNotFound(request, response);
     }
-    else if (request.url === "/usuario/list") {
-        response.writeHead(200, { "content-type": "application/json" })
-        response.write(JSON.stringify([{ username: "ana", active: false }, { username: "pepE", active: false }, { username: "eduardo2", active: true }, { username: "eduardo3", active: true }]));
-        response.end();
-    } else {
-        response.writeHead(404, { "content-type": "text/plain" });
-        response.write("Error - url no encontrada");
-        response.end();
-    }
-
 });
 
 server.listen(3001, () => console.log("Ejecutando en el puerto : 3000"));
