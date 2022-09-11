99dbde427e7e10ccae6bbad84615a58705bbb499
ARTURO EDUARDO FAJARDO GUTIÉRREZ (EXT) <efajardog-e@ripley.com.pe>
2022-09-10 21:27:42 -0500

    Validación de conexión con el servidor. con método AWAIT

--- src/index.ts
+++ src/index.ts
@@ -2,14 +2,32 @@ import ServerBootstrap, {Bootstrap} from "./boottrap/server.bootstrap";
 
 const serverBootstrap: Bootstrap = new ServerBootstrap();
 
+
+
+//Función de tipo ASYNC que permite validar la conexión con el servidor
+const start =async () => {
+    try
+    {
+        const resultServer = await serverBootstrap.initialize();
+        console.log(resultServer);
+    }
+    catch(error)
+    {
+        console.log(error);
+    }
+}
+
+//Ejecutando función async
+start();
+
+
 //serverBootstrap.initialize();
 //controlando la respuesta del servidor
-serverBootstrap
-.initialize()
-.then((message : string) =>  console.log(message))
-.catch((error : any) => console.log(error));
-
-
+//Se comentó para poner el ejemplo con el AWAIT
+// serverBootstrap
+// .initialize()
+// .then((message : string) =>  console.log(message))
+// .catch((error : any) => console.log(error));
 
 //Se comenta por motivos que se ha creado la clase BOOTSTRAP
 
